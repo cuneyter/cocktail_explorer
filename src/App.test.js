c8d5654f24c41fc1310fe11c1302ef1a980f4023
Cuneyt <cuneytergun@gmail.com>
2023-12-15 20:22:34 +0000

    Add App.js and App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { robots } from "./robots";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Component', () => {
+  test('renders App with heading and CardList', () => {
+    render(<App />);
+
+    // Check if the main heading is rendered
+    const headingElement = screen.getByText(/RoboFriends/i);
+    expect(headingElement).toBeInTheDocument();
+
+    // Check if the CardList component is rendered with robot cards
+    // Assuming that the robots array is not empty and each robot has a unique name
+    robots.forEach(robot => {
+      expect(screen.getByText(robot.name)).toBeInTheDocument();
+    });
+  });
 });
