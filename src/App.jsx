bd17a0cb1b90a0a81999da93e85a873d285d2601
Cuneyt <cuneytergun@gmail.com>
2023-12-16 17:42:08 +0000

    Add SearchBox.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { robots } from "./robots";
 import CardList from "./CardList";
+import SearchBox from "./SearchBox";
 
 const App = () => {
   return (
     <main className="tc">
       <h1>RoboFriends</h1>
+      <SearchBox />
       <CardList robots={robots} />
     </main>
   );
